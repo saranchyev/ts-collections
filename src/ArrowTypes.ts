1963aa39b2d150db2525bcbce40cf25f24340b7b
Ilya Saranchyev <engine-r@ya.ru>
2022-06-11 16:35:00 +0500

    adds some types for arrow functions (Predicate<T> and Comparer<T>)

---
+++ src/ArrowTypes.ts
@@ -0,0 +1,11 @@
+/**
+ *
+ * todo: fill JSDoc annotation
+ */
+export type Predicate<T> = (item: T, index?: number) => boolean
+
+/**
+ *
+ * todo: fill JSDoc annotation
+ */
+export type Comparer<T> = (a: T, b: T) => number
